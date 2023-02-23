d3e039fc017b78652d891b50c810936855a6bcca
kajalgore <kajalgore10@gmail.com>
2023-02-23 13:21:10 -0800

    gitbash setup demo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import AddData from './component/add';
+
+function App() {
+  return (
+    <div className="App">
+  <AddData />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/add.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AddData() {
+    return (
+      <div>
+       <h2>hello world</h2>
+      </div>
+    );
+  }
+  
+  export default AddData;
+  
